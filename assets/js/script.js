76efcb3ad0d2e165f5d7beec5a4f351610bc6baf
Jack Leonard-gill <u0zy2qef@students.codeinstitute.net>
2024-08-26 15:06:04 +0000

    Added array of players and player information

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let players = [
+    {name: "Erling Haaland", club: "Manchester City", position: "Striker", nationality: "Norway" },
+    {name: "Kevin De Bruyne", club: "Manchester City", position: "Midfielder", nationality: "Belgium" },
+    {name: "Mohamed Salah", club: "Liverpool", position: "Right Winger", nationality: "Egypt"},
+    {name: "Virgil Van Dijk", club: "Liverpool", position: "Centre-Back", nationality: "Netherlands" },
+    {name: "Cole Palmer", club: "Chelsea", position: "Forward", nationality: "England" },
+    {name: "Son Heung-min", club: "Tottenham", position: "Forward", nationality: "South Korea" },
+    {name: "Bukayo Saka", club: "Arsenal", position: "Right Winger", nationality: "England" },
+    {name: "Alisson Becker", club: "Liverpool", position: "Goalkeeper", nationality: "Brazil" },
+    {name: "William Saliba", club: "Arsenal", position: "Centre-Back", nationality: "France" },
+    {name: "Jamie Vardy", club: "Leicester", position: "Striker", nationality: "England" }
+]
